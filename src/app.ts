b9cc949c1f0141a690b689558706ec04f1e81dc7
eai04191 <eai@mizle.net>
2018-09-25 22:12:54 +0900

    First

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import Mastodon, { Status, StreamListener, Response } from "megalodon";
+const BASE_URL: string = "https://pcgamer.social";
+
+const access_token: string = "";
+
+const client = new Mastodon(access_token, BASE_URL + "/api/v1");
+
+const stream: StreamListener = client.stream("/streaming/user");
+
+stream.on("update", (status: Status) => {
+  console.log(status);
+  if (
+    status.account.username == "Eai" &&
+    status.tags.forEach(function(e) {
+      if (e.name == "pcgsinfo") {
+        return true;
+      }
+    })
+  ) {
+    client
+      .post<Status>(`/${status.id}/reblog`)
+      .then((res: Response<Status>) => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+});
+
+stream.on("heartbeat", () => {
+  console.log("thump.");
+});
